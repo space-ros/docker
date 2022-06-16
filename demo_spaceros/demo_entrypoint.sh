98f5e879af15d93503ef1aa787e0070eac410fb8
tonylitianyu <tonylitianyu@gmail.com>
2022-06-15 20:56:04 -0400

    added control packages
    Signed-off-by: tonylitianyu <tonylitianyu@gmail.com>

--- demo_spaceros/demo_entrypoint.sh
+++ demo_spaceros/demo_entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-# Setup the MoveIt2 environment
+# Setup the Demo environment
+source "/root/src/spaceros_ws/install/setup.bash"
+source "/root/src/depends_ws/install/setup.bash"
 source "/root/src/spaceros_demo_ws/install/setup.bash"
 exec "$@"
