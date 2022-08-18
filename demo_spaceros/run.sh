5f20e9d020983f44f71ccc69bcf1566211f4362b
Michael Jeronimo <michael.jeronimo@openrobotics.org>
2022-08-18 07:57:08 -0700

    Introduce a consistent set of run scripts (#22)
    Signed-off-by: Michael Jeronimo <michael.jeronimo@openrobotics.org>
    
    Signed-off-by: Michael Jeronimo <michael.jeronimo@openrobotics.org>

--- demo_spaceros/run.sh
+++ demo_spaceros/run.sh
@@ -1,1 +1,5 @@
-docker run --rm --gpus all --net=host -e DISPLAY=$DISPLAY --device=/dev/dri:/dev/dri --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -it openrobotics/spaceros-demo
+# Start the container
+docker run --rm --gpus all --net=host -e DISPLAY=$DISPLAY --device=/dev/dri:/dev/dri --volume="$HOME/.Xauthority:/home/spaceros-user/.Xauthority:rw" -it openrobotics/spaceros-demo
+
+# Launch the demo inside the container
+#   ros2 launch mars_rover mars_rover.launch.py
