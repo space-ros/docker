d3b2b5ad0380152eee2fe361ec7e29b6f256b056
Morgan Quigley <morgan@osrfoundation.org>
2022-11-07 12:50:04 -0500

    cross-compile Zenoh-Pico static library
    Signed-off-by: Morgan Quigley <morgan@osrfoundation.org>

--- zynq_rtems/enter_container.sh
+++ zynq_rtems/enter_container.sh
@@ -4,6 +4,7 @@ docker run --rm -e DISPLAY=$DISPLAY \
   --device=/dev/net/tun --cap-add=NET_ADMIN \
   --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
   --volume="$PWD/hello_network:/root/hello_network" \
+  --volume="$PWD/hello_zenoh:/root/hello_zenoh" \
   --network=host \
   -w /root \
   -it \
