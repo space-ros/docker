1b20e31fbf2c29281774dad3ad53954e3de2c9c5
Dharini Dutia <dharini@openrobotics.org>
2022-11-08 13:29:07 -0800

    cmake temp fix and rosinstall_generator)
    Signed-off-by: Dharini Dutia <dharini@openrobotics.org>

--- moveit2_tutorials/run.sh
+++
@@ -1,5 +0,0 @@
-# Start the container
-docker run --rm --gpus all --net=host -e DISPLAY=$DISPLAY --device=/dev/dri:/dev/dri --volume="$HOME/.Xauthority:/home/spaceros-user/.Xauthority:rw" -it openrobotics/moveit2_tutorials
-
-# Launch the tutorial inside the container
-#   ros2 launch moveit2_tutorials demo.launch.py rviz_tutorial:=true
