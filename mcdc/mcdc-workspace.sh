dd24d835f2b21089fb90ca598cd20e0a527bf256
Ian Chen <ichen@osrfoundation.org>
2022-12-02 20:19:01 -0800

    Add scripts for running the mcdc_checker and converting the output to sarif. Updated Earthfile to call the new run_mcdc_checker.py script
    Signed-off-by: Ian Chen <ichen@osrfoundation.org>

--- mcdc/mcdc-workspace.sh
+++ mcdc/mcdc-workspace.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
+mcdc_checker_script=`pwd`/run_mcdc_checker.py
 for packagedir in $@; do
 	pushd $package
-	mcdc_checker -a 2>&1 | tee mcdc-results.txt
+	python3 $mcdc_checker_script `pwd` --sarif_file mcdc-results.txt --verbose
 	popd
 done
 
