7bd8af7774e872d622a003d36ed44298eb1b8411
Morgan Quigley <morgan@osrfoundation.org>
2022-12-11 00:21:18 -0500

    branch update and ros2 path update
    Signed-off-by: Morgan Quigley <morgan@osrfoundation.org>

--- zynq_rtems/enter_container.sh
+++ zynq_rtems/enter_container.sh
@@ -5,7 +5,7 @@ docker run --rm -e DISPLAY=$DISPLAY \
   --volume="$PWD/hello_network:/root/hello_network" \
   --volume="$PWD/hello_zenoh:/root/hello_zenoh" \
   --volume="$PWD/diagnostics_pub:/root/diagnostics_pub" \
-  --volume="$PWD/static_ros2:/root/static_ros2" \
+  --volume="$PWD/ros2:/root/ros2" \
   --network=host \
   -w /root \
   -it \
