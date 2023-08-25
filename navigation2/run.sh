1cf281bce417210641848a4aeaae0fbbd4f87c15
Matthew Hansen <12618239+mkhansenbot@users.noreply.github.com>
2023-08-25 21:58:18 +0000

    Use rocker to run docker image in run script (#126).

--- navigation2/run.sh
+++ navigation2/run.sh
@@ -1,2 +1,21 @@
+#!/usr/bin/env bash
+
+# Runs a docker container with the image created by build.bash
+# Requires:
+#   docker
+#   an X server
+#   rocker
+
+
+ROCKER_ARGS="--devices /dev/dri/ --dev-helpers --x11 --user-override-name spaceros-user --network host"
+
+IMG_NAME=osrf/space_nav2
+
+# Replace `/` with `_` to comply with docker container naming
+# And append `_runtime`
+CONTAINER_NAME="$(tr '/' '_' <<< "$IMG_NAME")"
+ROCKER_ARGS="${ROCKER_ARGS} --name $CONTAINER_NAME"
+echo "Using image <$IMG_NAME> to start container <$CONTAINER_NAME>"
+
 # Start the container
-docker run --rm --gpus all --net=host -e DISPLAY=$DISPLAY --device=/dev/dri:/dev/dri --volume="$HOME/.Xauthority:/home/spaceros-user/.Xauthority:rw" -it openrobotics/navigation2
+rocker ${ROCKER_ARGS} $IMG_NAME
