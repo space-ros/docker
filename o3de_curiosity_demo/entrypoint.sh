97d520f3d026eb0b0f714a6032f8fd29e689e269
Mechazo11 <azmyin12@gmail.com>
2024-09-09 17:41:50 -0500

    space ros ws, moveit2 ws, demo_ws builds, need to fix o3de build

--- o3de_curiosity_demo/entrypoint.sh
+++ o3de_curiosity_demo/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-# Setup the Demo environment
+# Source all relevant worksapces
+source "${SPACEROS_DIR}/install/setup.bash"
+source "${MOVEIT2_DIR}/install/setup.bash"
 source "${DEMO_DIR}/install/setup.bash"
 exec "$@"
--- o3de_curiosity_demo/run.sh
+++ o3de_curiosity_demo/run.sh
@@ -5,12 +5,25 @@
 #   docker
 #   an X server
 
-IMG_NAME=openrobotics/space_robots_demo
+IMG_NAME=openrobotics/o3de_curiosity_docker
 
 # Replace `/` with `_` to comply with docker container naming
 # And append `_runtime`
 CONTAINER_NAME="$(tr '/' '_' <<< "$IMG_NAME")"
 
+# TODO this needs to be updated with 
 # Start the container
 docker run --rm -it --name $CONTAINER_NAME  --network host \
     -e DISPLAY -e TERM   -e QT_X11_NO_MITSHM=1 $IMG_NAME
+
+# xhost +local:docker
+
+# docker run -d \
+# --name=o3de_rosbot_xl \
+# --runtime=nvidia \
+# -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+# -e DISPLAY=${DISPLAY} \
+# -e NVIDIA_VISIBLE_DEVICES=all \
+# -e NVIDIA_DRIVER_CAPABILITIES=all \
+# o3de-husarion /data/workspace/ROSbotXLDemo/build/linux/bin/profile/Editor
+
