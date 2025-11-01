0347a849ee9a0fd93e5b541354ad2efac18a6fca
David Sobek <david.sobek@picknik.ai>
2025-10-31 19:20:51 -0600

    Update stacks to jazzy-2025.10.0 (#264)

--- navigation2/build.sh
+++ navigation2/build.sh
@@ -17,7 +17,7 @@ echo ""
 docker build -t $ORG/$IMAGE:$TAG \
     --build-arg VCS_REF="$VCS_REF" \
     --build-arg VERSION="$VERSION" \
-    --build-arg SPACE_ROS_IMAGE="${SPACE_ROS_IMAGE:-osrf/space-ros:jazzy-2025.07.0}" \
+    --build-arg SPACE_ROS_IMAGE="${SPACE_ROS_IMAGE:-osrf/space-ros:jazzy-2025.10.0}" \
     .
 
 echo ""
